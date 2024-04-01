986037dcc8c607a547bc886915112a13e7b25277
suezhalder <szhldr@gmail.com>
2024-04-01 15:07:17 +0600

    specialties and doctorSpecialties schema added

---
+++ prisma/migrations/20240401090607_doctor_specialties/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "specialties" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "icon" TEXT NOT NULL,
+
+    CONSTRAINT "specialties_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "doctor_specialties" (
+    "specialtiesId" TEXT NOT NULL,
+    "doctorId" TEXT NOT NULL,
+
+    CONSTRAINT "doctor_specialties_pkey" PRIMARY KEY ("specialtiesId","doctorId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "specialties_id_key" ON "specialties"("id");
+
+-- AddForeignKey
+ALTER TABLE "doctor_specialties" ADD CONSTRAINT "doctor_specialties_specialtiesId_fkey" FOREIGN KEY ("specialtiesId") REFERENCES "specialties"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "doctor_specialties" ADD CONSTRAINT "doctor_specialties_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "doctors"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
