40265c4e5f06560ba961669614ce6ff15a52dae8
suezhalder <szhldr@gmail.com>
2024-04-09 22:10:54 +0600

    schedule created

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,5 +1,7 @@
 //* src/app/modules/Schedule/schedule.service.ts
-import { addHours, format } from "date-fns";
+
+import { addHours, addMinutes, format } from "date-fns";
+import prisma from "../../../shared/prisma";
 
 // * -------------------------- * //
 //!  Create Schedule
@@ -8,8 +10,12 @@ import { addHours, format } from "date-fns";
 const createScheduleIntoDB = async (payload: any) => {
     const { startDate, endDate, startTime, endTime } = payload;
 
-    const currentDate = new Date(startDate);
-    const lastDate = new Date(endDate);
+    const intervalTime = 30;
+
+    const schedules = [];
+
+    const currentDate = new Date(startDate); // start date
+    const lastDate = new Date(endDate); // end date
 
     while (currentDate <= lastDate) {
         const startDateTime = new Date(
@@ -21,15 +27,31 @@ const createScheduleIntoDB = async (payload: any) => {
 
         const endDateTime = new Date(
             addHours(
-                `${format(lastDate, "yyyy-MM-dd")}`,
+                `${format(currentDate, "yyyy-MM-dd")}`,
                 Number(endTime.split(":")[0])
             )
         );
 
-        while (startDateTime <= endDateTime) {
+        while (startDateTime < endDateTime) {
             // create 30 mins interval
+            const scheduleData = {
+                startDateTime: startDateTime,
+                endDateTime: addMinutes(startDateTime, intervalTime),
+            };
+
+            // console.log(scheduleData);
+            const result = await prisma.schedule.create({
+                data: scheduleData,
+            });
+            schedules.push(result);
+
+            startDateTime.setMinutes(startDateTime.getMinutes() + intervalTime);
         }
+
+        currentDate.setDate(currentDate.getDate() + 1);
     }
+
+    return schedules;
 };
 
 export const ScheduleService = {
