98e7a72afc392217c3c02f55e982b399b4f32f4e
suezhalder <szhldr@gmail.com>
2024-04-12 12:31:23 +0600

    fixed bug

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 //* src/app.ts
 
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
