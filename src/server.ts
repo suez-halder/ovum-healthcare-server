f38d19392855dedf892b7e496b1378190169ba9e
suezhalder <szhldr@gmail.com>
2024-04-17 19:18:58 +0600

    handled uncaughtException and unhandledRejection

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,27 @@ async function main() {
     const server: Server = app.listen(config.port, () => {
         console.log("Server is running on port", config.port);
     });
+
+    const exitHandler = () => {
+        if (server) {
+            server.close(() => {
+                console.info("Server closed!");
+            });
+        }
+        process.exit(1);
+    };
+
+    //? server jodi crush korte ney, tokhn uncaughtException er maddhome server off kore dite hoy
+    process.on("uncaughtException", (error) => {
+        console.log(error);
+        exitHandler();
+    });
+
+    //? Try-Catch jokhn kono error dhorte parena tokhn unhandledRejection hoy
+    process.on("unhandledRejection", (error) => {
+        console.log(error);
+        exitHandler();
+    });
 }
 
 main();
