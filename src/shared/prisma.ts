83f29513b266bbe11801ebd205ca9a47e163ed5e
suezhalder <szhldr@gmail.com>
2024-04-17 19:10:53 +0600

    enabled prisma logging

--- src/shared/prisma.ts
+++ src/shared/prisma.ts
@@ -2,6 +2,39 @@
 
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// ------------------------------ //
+//! Enabling Prisma Logging
+// ------------------------------ //
+
+const prisma = new PrismaClient({
+    log: [
+        {
+            emit: "event",
+            level: "query",
+        },
+        {
+            emit: "stdout",
+            level: "error",
+        },
+        {
+            emit: "stdout",
+            level: "info",
+        },
+        {
+            emit: "stdout",
+            level: "warn",
+        },
+    ],
+});
+
+// prisma.$on("query", (e) => {
+//     console.log("----------------------------");
+//     console.log("Query: " + e.query);
+//     console.log("----------------------------");
+//     console.log("Params: " + e.params);
+//     console.log("----------------------------");
+//     console.log("Duration: " + e.duration + "ms");
+//     console.log("----------------------------");
+// });
 
 export default prisma;
